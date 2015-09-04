1e3697ea30862058719233295123a600c9d585c6
Daniel Naumann <d.naumann@yatego.com>
2015-09-04 02:16:15 +0200

    last try

--- src/DevGarden/simpleq/SimpleqBundle/Command/WorkingQueueHistoryCommand.php
+++ src/DevGarden/simpleq/SimpleqBundle/Command/WorkingQueueHistoryCommand.php
@@ -4,6 +4,8 @@ namespace DevGarden\simpleq\SimpleqBundle\Command;
 
 class WorkingQueueHistoryCommand extends BaseCommand
 {
+    CONST CODE_CLIMAX_DUPLICATE_CONTENT_DUMMY = 'levenshtein sucks';
+
     public function configure()
     {
         $this->setName(\CommandPatterns::SCHEDULER_HISTORY);
