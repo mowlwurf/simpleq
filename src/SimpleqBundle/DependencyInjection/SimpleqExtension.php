eb183da85ef136500b296f2d518ee2c19aa6e86e
Daniel Naumann <d.naumann@yatego.com>
2016-11-18 18:31:03 +0100

    remove weird BaseExtension

--- src/SimpleqBundle/DependencyInjection/SimpleqExtension.php
+++ src/SimpleqBundle/DependencyInjection/SimpleqExtension.php
@@ -2,12 +2,17 @@
 
 namespace simpleq\SimpleqBundle\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class SimpleqExtension extends BaseExtension
+class SimpleqExtension extends Extension
 {
     /**
      * {@inheritdoc}
